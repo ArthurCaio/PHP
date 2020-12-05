14b5c2f0c34055f770f7ab59cd1e6d2b33a3cd7f
ArthurCaio <arthurcaiosantoslins@gmail.com>
2020-12-04 21:10:01 -0300

    Update-PHP-01

--- 01.php
+++ 01.php
@@ -1,3 +1,15 @@
-<?php
-echo "Olá, Mundo";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // escrever na tela é so usar o 'echo' ou 'print'
+        print "Meu nome é Arthur<br>";
+        echo "Minha idade é de 14 anos";
+    ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo "<a href='01.php'>Aula 01</a>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01 fundamentos basicos</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Aulas</h1>";
+        echo "<a href='01.php'>Aula 01</a>";
+    ?>
+</body>
+</html>

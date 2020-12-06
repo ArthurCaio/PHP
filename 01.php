b5052af3d48ac14a52f1595df4836d5f4d7b7dcf
ArthurCaio <arthurcaiosantoslins@gmail.com>
2020-12-06 15:35:29 -0300

    Update-04

--- 01.php
+++ 01.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        // escrever na tela é so usar o 'echo' ou 'print'
-        print "Meu nome é Arthur<br>";
-        echo "Minha idade é de 14 anos";
-    ?>
-</body>
-</html>
+
+<?php
+    // escrever na tela é so usar o 'echo' ou 'print'
+    print "Meu nome é Arthur<br>";
+    echo "Minha idade é de 14 anos";
+?>
--- index.php
+++ index.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>01 fundamentos basicos</title>
-</head>
-<body>
-    <?php
-        echo "<h1>Aulas</h1>";
-        echo "<a href='01.php'>Aula 01</a>";
-        print "<br>";
-        echo "<a href='02.php'>Aula 02</a>";
-        print "<br>";
-        echo "<a href='03.php'>Aula 03</a>";
-        print "<br>";
-        echo "<a href='04.php'>Aula 04</a>";
-        print "<br>";
-        echo "<a href='05.php'>Aula 05</a>";
-    ?>
-</body>
-</html>
+<?php
+    echo "<h1>Aulas</h1>";
+    echo "<a href='01.php'>Aula 01</a>";
+    print "<br>";
+    echo "<a href='02.php'>Aula 02</a>";
+    print "<br>";
+    echo "<a href='03.php'>Aula 03</a>";
+    print "<br>";
+    echo "<a href='04.php'>Aula 04</a>";
+    print "<br>";
+    echo "<a href='05.php'>Aula 05</a>";
+?>

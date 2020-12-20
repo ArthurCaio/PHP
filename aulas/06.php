17373af8b5504e3838a4ad7f102b6582913b9b4f
ArthurCaio <arthurcaiosantoslins@gmail.com>
2020-12-20 18:36:30 -0300

    Update-index.php

--- aulas/06.php
+++ aulas/06.php
@@ -1,6 +1,34 @@
 <?php
+    $a = 1;
+    $b = 5;
+    $c = 7;
+    ///////////////////////
     $nome = 'Arthur Caio';
     function exibirnome(){
+        global $nome;
         print $nome;
     }
+    exibirnome();
+    echo "<hr>";
+    //////////////////////
+    function cidade(){
+        global $cidade;
+        $cidade = 'Paulista';
+    }
+    cidade();
+    echo $cidade;
+    //////////////////////
+    function soma(){
+        /*
+        global $soma;
+        global $a;
+        global $b;
+        global $c;
+        $soma = $a + $b + $c;
+        echo " A soma de $a, $b e $c é igual á $soma";
+        */
+        echo $GLOBALS['a'] + $GLOBALS['b'] + $GLOBALS['c'];
+    }
+    echo "<hr>";
+    soma();
 ?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo"
+            <a href='aulas/01.php'>Aula 1</a><br>
+            <a href='aulas/02.php'>Aula 2</a><br>
+            <a href='aulas/03.php'>Aula 3</a><br>
+            <a href='aulas/04.php'>Aula 4</a><br>
+            <a href='aulas/05.php'>Aula 5</a><br>
+            <a href='aulas/06.php'>Aula 6</a><br>
+            <a href='aulas/07.php'>Aula 7</a><br>
+            <a href='aulas/08.php'>Aula 8</a><br>
+        ";
+    ?>
+</body>
+</html>
